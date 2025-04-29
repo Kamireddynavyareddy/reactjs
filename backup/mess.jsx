bc0471c8d002e5df708b000d7163d0d745b44ebc
Kamireddynavyareddy <navyareddy3777@gmail.com>
2025-04-29 13:16:25 +0530

    reactjs

---
+++ backup/mess.jsx
@@ -0,0 +1,10 @@
+import react from 'react'
+import './message.css'
+let meassage =()=>{
+
+    return <react.Fragment>
+        <h3>message component</h3>
+        <h4 className='bg-color'>good morning</h4>
+    </react.Fragment>
+}
+export default Message;
---
+++ backup/message.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+let Message = ()=>{
+
+    let myStyles={backgroundColor:"yellow"}
+    return <React.Fragment>
+            <h3>Message Component</h3>
+            <h3 style={myStyles}>GM</h3>
+            <h3>GA</h3>
+
+           </React.Fragment>
+}
+export default Message;
