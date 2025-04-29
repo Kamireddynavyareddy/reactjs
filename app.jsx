fe2d3384cc0b62b5941991d7944725e9df026301
Kamireddynavyareddy <navyareddy3777@gmail.com>
2025-04-29 12:34:48 +0530

    reactjs

---
+++ app.jsx
@@ -0,0 +1,11 @@
+import react,{component}from "react";
+class Message extends react.component{
+
+    render(){
+        return <div>
+
+            <h4>meassage component</h4>
+        </div>
+    }
+}
+export default Message;
